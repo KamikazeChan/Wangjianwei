963fae1c6af07b5c02777b78e36b3aa5a55485ed
chaoxchan <chaoxtan@gmail.com>
2020-09-05 22:54:11 +0800

    changelog : 3.1

---
+++ src/copy.cpp
@@ -0,0 +1,12 @@
+//
+// Created by ³ÂÅæÄĞ on 5/9/2020.
+//
+
+#include "copy.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+}
---
+++ src/copy.h
@@ -0,0 +1,8 @@
+//
+// Created by ³ÂÅæÄÐ on 5/9/2020.
+//
+
+#ifndef WANGJIANWEI_COPY_H
+#define WANGJIANWEI_COPY_H
+
+#endif //WANGJIANWEI_COPY_H
